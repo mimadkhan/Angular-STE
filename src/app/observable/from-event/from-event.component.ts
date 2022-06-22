44040740503be393825564d30b47861802e35283
mimadkhan <mimadkhansgr@gmail.com>
2022-06-23 01:34:01 +0500

    update fromEvent

--- src/app/observable/from-event/from-event.component.ts
+++ src/app/observable/from-event/from-event.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { DesignService } from 'src/app/service/design.service';
 
 @Component({
   selector: 'app-from-event',
   templateUrl: './from-event.component.html',
   styleUrls: ['./from-event.component.css']
 })
-export class FromEventComponent implements OnInit {
+export class FromEventComponent implements OnInit,AfterViewInit {
+ 
+  @ViewChild('addBtn') addBtn!:ElementRef;
+  
+  constructor(private designSrv : DesignService) { }
 
-  constructor() { }
+  ngAfterViewInit(): void {
+    let count = 0;
+    fromEvent(this.addBtn.nativeElement,'click').subscribe((res)=>{
+      let resValue = 'Video ' + count++;
+      // console.log('Video '+ count++);
+      this.designSrv.print(resValue,'elContainer');
+      this.designSrv.print(resValue,'elContainer2');
+    });  }
 
   ngOnInit(): void {
   }
- 
+
+  // print(val: string, containerId: string){
+  //   let el = document.createElement('li');
+  //   el.innerText= 'RXJS Series ' + val;
+  //   document.getElementById(containerId)?.appendChild(el);
+  // }
 }
---
+++ src/app/service/design.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DesignService {
+
+  constructor() { }
+
+  print(val: string, containerId: string){
+    let el = document.createElement('li');
+    el.innerText= 'RXJS Series ' + val;
+    document.getElementById(containerId)?.appendChild(el);
+  }
+}
